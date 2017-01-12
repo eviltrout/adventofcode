d26476f7d63460a51040926fc14a80d9e5fcd5a7
Robin Ward <robin.ward@gmail.com>
2017-01-11 21:05:54 -0500

    Day 15

--- 2016/day15.rb
+++ 2016/day15.rb
@@ -1,29 +1,25 @@
-# discs = [{size: 5, current: 4}, {size: 2, current: 1}]
-
 discs = [
-  {size: 17, current: 5},
-  {size: 19, current: 8},
-  {size: 7, current: 1},
-  {size: 13, current: 7},
-  {size: 5, current: 1},
-  {size: 3, current: 0},
+  {size: 17, current: 5, pos: 1},
+  {size: 19, current: 8, pos: 2},
+  {size: 7, current: 1, pos: 3},
+  {size: 13, current: 7, pos: 4},
+  {size: 5, current: 1, pos: 5},
+  {size: 3, current: 0, pos: 6},
+  {size: 11, current: 0, pos: 7},
 ]
 
 t = 0
-
 while true
-  all = true
+  t += 1
+
+  found = 0
   discs.each_with_index do |d, i|
-    all = all && ((d[:current] + t) % d[:size]) == i
-    break unless all
+    break unless ((d[:current] + t + d[:pos]) % d[:size]) == 0
+    found += 1
   end
-  if all
-    puts "Found #{t-1}"
-    break
+
+  if found == discs.size
+    puts "---> #{t}"
+    exit
   end
-  t += (3 * 5 * 13 * 7 * 19 * 17)
-  
-  puts t if (t % 1000) == 0
 end
-
-
