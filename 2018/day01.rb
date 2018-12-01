1132c90b2eb4ffcf797250f43ecb1b0366eda575
Robin Ward <robin.ward@gmail.com>
2018-12-01 17:32:07 -0500

    Day 1 solution

---
+++ 2018/day01.rb
@@ -0,0 +1,19 @@
+vals = File.readlines('day01.input').map { |x| x.chomp.to_i }
+
+# Part One
+puts vals.sum
+
+# Part Two
+prev_sums = [0]
+
+sum = 0
+loop do
+  vals.each do |v|
+    sum += v
+    if prev_sums.include?(sum)
+      puts sum
+      exit
+    end
+    prev_sums << sum
+  end
+end
