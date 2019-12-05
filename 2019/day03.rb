50b888eabcf57b752b3cbd3a3d2f54737722d4fa
Robin Ward <robin.ward@gmail.com>
2019-12-05 13:44:06 -0500

    Finished day03

--- 2019/day03.rb
+++ 2019/day03.rb
@@ -1,61 +1,45 @@
 input = File.read('day03.input')
 
-v_lines = []
-h_lines = []
-
+# input = "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\nU98,R91,D20,R16,D67,R40,U7,R15,U6,R7"
 wires = input.split("\n")
 
-x, y = 0, 0
-wires[0].split(",").each do |inst|
-  dir, length = inst[0], inst[1..-1].to_i
+grids = [ {}, {} ]
+distances = []
+steps = []
 
-  case dir
-  when 'U'
-    v_lines << [x, y - length, y]
-    y = y - length
-  when 'D'
-    v_lines << [x, y, y + length]
-    y = y + length
-  when 'L'
-    h_lines << [y, x - length, x]
-    x = x - length
-  when 'R'
-    h_lines << [y, x, x + length]
-    x = x + length
-  end
-end
+wires.each_with_index do |wire, i|
+  x, y = 0, 0
+  travelled = 0
+  move = ->(x1, y1) {
+    x, y = x1, y1
+    travelled += 1
+    grids[i][y] ||= {}
+    grids[i][y][x] = travelled if grids[i][y][x].nil?
 
-def intersect(lines, a, b0, b1)
-  distances = []
-  lines.each do |l|
-    if l[0] > b0 && l[0] < b1 && (a > l[1] && a < l[2])
-      distances << (l[0].abs + a.abs)
+    grids[0][y] ||= {} if i == 1
+
+    if i == 1 && !grids[0][y][x].nil?
+      distances << x.abs + y.abs
+      steps << (travelled + (grids[0][y][x] || 0))
+    end
+  }
+
+  wire.split(',').each do |inst|
+    dir, length = inst[0], inst[1..-1].to_i
+
+    case dir
+    when 'U'
+      length.times { move.call(x, y - 1) }
+    when 'D'
+      length.times { move.call(x, y + 1) }
+    when 'L'
+      length.times { move.call(x - 1, y) }
+    when 'R'
+      length.times { move.call(x + 1, y) }
     end
   end
-  distances.min
 end
 
-x, y = 0, 0
-distances = []
-d = nil
-wires[1].split(",").each do |inst|
-  dir, length = inst[0], inst[1..-1].to_i
-
-  case dir
-  when 'U'
-    distances << d if d = intersect(h_lines, x, y - length, y)
-    y = y - length
-  when 'D'
-    distances << d if d = intersect(h_lines, x, y, y + length)
-    y = y + length
-  when 'L'
-    distances << d if d = intersect(v_lines, y, x - length, x)
-    x = x - length
-  when 'R'
-    distances << d if d = intersect(v_lines, y, x, x + length)
-    x = x + length
-  end
-end
-puts distances.inspect
 puts distances.min
+puts steps.min
 
