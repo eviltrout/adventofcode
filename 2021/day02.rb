80af76dd686e9b4f408de6413ef10b80fbd4deae
Robin Ward <robin.ward@gmail.com>
2021-12-02 10:17:05 -0500

    Fix whitespace

--- 2021/day02.rb
+++ 2021/day02.rb
@@ -6,9 +6,9 @@ vals.split("\n").each do |i|
   n = n.to_i
   if cmd == "forward"
     h += n
-		d += (a * n)
+    d += (a * n)
   else
-		a += (cmd == "up") ? -n : n
+    a += (cmd == "up") ? -n : n
   end
 end
 
