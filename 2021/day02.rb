8016c24f38a1783e678f55e3457e92a6679d724d
Robin Ward <robin.ward@gmail.com>
2021-12-02 10:10:27 -0500

    Easy peasy

---
+++ 2021/day02.rb
@@ -0,0 +1,18 @@
+vals = File.read(File.basename(__FILE__, ".rb") + ".input")
+
+h = a = d = 0
+vals.split("\n").each do |i|
+  cmd, n = i.split
+  n = n.to_i
+  if cmd == "forward"
+    h += n
+		d += (a * n)
+  else
+		a += (cmd == "up") ? -n : n
+  end
+end
+
+puts h * a
+puts h * d
+
+
