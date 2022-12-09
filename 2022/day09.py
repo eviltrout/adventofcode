27fd4e91a2141db8b35476f10a52bacdece7c8c7
Robin Ward <robin.ward@gmail.com>
2022-12-09 13:43:41 -0500

    Not so bad in the end

--- 2022/day09.py
+++ 2022/day09.py
@@ -19,39 +19,29 @@ R 2
 """)
 
 base = os.path.splitext(os.path.basename(__file__))[0]
-# data = open(base + ".input").read()
+data = open(base + ".input").read()
 
 deltas = { 'U': (0, -1), 'D': (0, 1), 'L': (-1, 0), 'R': (1, 0) }
 neighbors = [(x, y) for x in range(-1, 2) for y in range(-1, 2)]
 
-def tail_far(tail, head):
-    for n in neighbors:
-        if head == (tail[0] + n[0], tail[1] + n[1]):
-            return False
-    return True
+def normalize(x):
+    return (-1 if x < 0 else 1 if x > 0 else 0)
 
-prev_head = (0, 0)
-head = (0, 0)
-tail = (0, 0)
-visited = set()
-visited.add(tail)
-for l in data.splitlines():
-    d, c = l.split(' ')
+def find_visited(rope_size):
+    rope = [(0, 0) for x in range(0, rope_size)]
+    visited = set()
+    visited.add(rope[-1])
+    for l in data.splitlines():
+        d, c = l.split(' ')
 
-    for i in range(int(c)):
-        prev_head = (head[0], head[1])
-        head = (head[0] + deltas[d][0], head[1] + deltas[d][1])
+        for i in range(int(c)):
+            rope[0] = (rope[0][0] + deltas[d][0], rope[0][1] + deltas[d][1])
+            for i in range(1, rope_size):
+                if len([n for n in neighbors if rope[i-1] == (rope[i][0] + n[0], rope[i][1] + n[1])]) <= 0:
+                    diff = (normalize(rope[i-1][0] - rope[i][0]), normalize(rope[i-1][1] - rope[i][1]))
+                    rope[i] = (rope[i][0] + diff[0], rope[i][1] + diff[1])
+            visited.add(rope[-1])
+    return visited
 
-        if tail_far(tail, head):
-            tail = (prev_head[0], prev_head[1])
-            visited.add(tail)
-
-print("map")
-for y in range(-10, 10):
-    for x in range(-10, 10):
-        if (x, y) in visited:
-            print("*", end="")
-        else:
-            print(".", end="")
-    print()
-print(len(visited))
+print(len(find_visited(2)))
+print(len(find_visited(10)))
