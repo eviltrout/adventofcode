5eba11f5bd573a0d8de5aec12aae8863e71cd4f1
Robin Ward <robin.ward@gmail.com>
2022-12-21 09:05:49 -0500

    Day 16 p1

--- 2022/day16.py
+++ 2022/day16.py
@@ -21,77 +21,88 @@ Valve JJ has flow rate=21; tunnel leads to valve II
 """)
 
 base = os.path.splitext(os.path.basename(__file__))[0]
-# data = open(base + ".input").read()
+data = open(base + ".input").read()
 
 valves = {}
+targets = set()
 for l in data.splitlines():
-	id = l[6:8]
-	flow = int(re.search("rate=(\d+);", l).group(1))
-	exits = l[re.search('valves? ', l).end(0):].split(', ')
-	valves[id] = { 'id': id, 'flow': flow, 'exits': exits }
+    id = l[6:8]
+    flow = int(re.search("rate=(\d+);", l).group(1))
+    if flow > 0:
+        targets.add(id)
+    exits = l[re.search('valves? ', l).end(0):].split(', ')
+    valves[id] = { 'id': id, 'flow': flow, 'exits': exits }
 
-class History:
-	def __init__(self):
-		self.current = 'AA'
-		self.steps = 0
-		self.total = 0
-		self.flow = 0
-		self.activated = set()
+def map_targets(id):
+    to_visit = [[id]]
+    visited = set([id])
+    depth = 0
+    path = []
+    sub_targets = {}
+    while len(to_visit) > 0:
+        depth += 1
+        path = to_visit.pop(0)
+        for e in valves[path[-1]]['exits']:
+            if e not in visited:
+                visited.add(e)
+                new_path = copy.copy(path)
+                new_path.append(e)
+                to_visit.append(new_path)
 
-	def move_to(self, id):
-		self.current = id
-		self.steps += 1
+                if e in targets:
+                    sub_targets[e] = new_path
 
-	def activate(self, flow):
-		self.activated.add(self.current)
-		self.flow += flow
-		self.steps += 1
+    valves[id]['sub_targets'] = sub_targets
 
-	def clone(self):
-		result = History()
-		result.current = self.current
-		result.steps = self.steps
-		result.flow = self.flow
-		result.total = self.total
-		result.activated = copy.deepcopy(self.activated)
-		return result
+for id in valves:
+    map_targets(id)
 
-seen = {}
-max_step = {}
+class State:
+    def __init__(self):
+        self.current = 'AA'
+        self.total = 0
+        self.flow = 0
+        self.min = 0
+        self.valves_left = copy.copy(targets)
 
-def run(history):
-	if history.steps == 30:
-		return history
+    def clone(self):
+        result = State()
+        result.total = self.total
+        result.flow = self.flow
+        result.min = self.min
+        result.valves_left = copy.copy(self.valves_left)
+        return result
 
-	# key = str(history.steps) + ":" + str(history.current) + ":" + str(history.total) + ":".join(sorted(list(history.activated)))
-	# if key in seen:
-	# 	return seen[key]
+    def wait(self, n):
+        for i in range(n):
+            self.min += 1
+            self.total += self.flow
 
-	history.total += history.flow
-	if history.steps in max_step:
-		tmp = max_step[history.steps]
-		if tmp.total > history.total:
-			return tmp
-	max_step[history.steps] = history.clone()
+    def finish(self):
+        while self.min < 30:
+            self.total += self.flow
+            self.min += 1
+        return self
 
-	result = history
 
-	valve = valves[history.current]
-	if valve['flow'] > 0 and (valve['id'] not in history.activated):
-		new_history = history.clone()
-		new_history.activate(valve['flow'])
-		tmp = run(new_history)
-		if tmp.total > result.total:
-			result = tmp
+def find_best(state):
+    valve = valves[state.current]
+    if len(state.valves_left) == 0:
+        return state.finish()
+    else:
+        best = state.clone()
+        for dest in state.valves_left:
+            new_state = state.clone()
+            path = valve['sub_targets'][dest][1:]
+            new_state.wait(len(path) + 1)
+            new_state.current = dest
+            new_state.flow += valves[dest]['flow']
+            new_state.valves_left.remove(dest)
+            if new_state.min <= 30:
+                result = find_best(new_state)
+                if result.total > best.total:
+                    best = result
+        return best
 
-	for e in valve['exits']:
-		new_history = history.clone()
-		new_history.move_to(e)
-		tmp = run(new_history)
-		if tmp.total > result.total:
-			result = tmp
-
-	return result
-
-winner = run(History())
+winner = find_best(State())
 print(winner.total)
