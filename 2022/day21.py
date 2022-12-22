6be21c8ddcf3321fbe675ca2c2121a4e64f24e86
Robin Ward <robin.ward@gmail.com>
2022-12-22 14:44:03 -0500

    Why no work?

--- 2022/day21.py
+++ 2022/day21.py
@@ -1,6 +1,7 @@
 from inspect import cleandoc
 from itertools import combinations
 
+import sys
 import copy
 import re
 import os
@@ -26,7 +27,8 @@ hmdt: 32
 """)
 
 base = os.path.splitext(os.path.basename(__file__))[0]
-# data = open(base + ".input").read()
+data = open(base + ".input").read()
+sys.setrecursionlimit(3000)
 
 cmds = {}
 for l in data.splitlines():
@@ -34,40 +36,75 @@ for l in data.splitlines():
 
     m = re.match("([a-z]{4}) ([+-/*]) ([a-z]{4})", cmd)
     if m is None:
-        cmds[id] = { 'type': 'scalar', 'value': int(cmd) }
+        cmds[id] = { 'type': 'scalar', 'value': float(cmd) }
     else:
         cmds[id] = { 'type': 'op', "l": m.group(1), "op": m.group(2), "r": m.group(3) }
 
-def solve(cmds, id):
-    n = cmds[id]
-    if n['type'] == 'scalar':
-        return n['value']
+def build_tree(cmds, id):
+    node = cmds[id]
+    node['id'] = id
 
-    l = solve(cmds, n['l'])
-    r = solve(cmds, n['r'])
-    
-    if n['op'] == '+':
+    if node['type'] == 'scalar':
+        return node
+
+    node['l'] = build_tree(cmds, node['l'])
+    node['r'] = build_tree(cmds, node['r'])
+    return node
+
+reverse = { '+': '-', '-': '+', '/': '*', '*': '/' }
+
+def perform(l, r, op):
+    if op == '+':
         return l + r
-    elif n['op'] == '-':
+    elif op == '-':
         return l - r
-    elif n['op'] == '/':
-        return l // r
-    elif n['op'] == '*':
+    elif op == '/':
+        return l / r
+    elif op == '*':
         return l * r
 
-def formula(cmds, id):
-    if id == 'humn':
+def solve(n, h = None):
+    if h is not None and n['id'] == 'humn':
+        return h
+    if n['type'] == 'scalar':
+        return n['value']
+    return perform(solve(n['l'], h), solve(n['r'], h), n['op'])
+
+def formula(node):
+    if id in node and node['id'] == 'humn':
         return 'x'
 
-    n = cmds[id]
-    if n['type'] == 'scalar':
-        return str(n['value'])
+    if node['type'] == 'scalar':
+        return str(node['value'])
+    elif node['type'] == 'variable':
+        return node['name']
 
-    l = formula(cmds, n['l'])
-    r = formula(cmds, n['r'])
-    return "(" + l + " " + n['op'] + " " + r + ")"
+    l = formula(node['l'])
+    r = formula(node['r'])
+    return "(" + l + " " + node['op'] + " " + r + ")"
 
-print(solve(cmds, "root"))
-print(formula(cmds, cmds["root"]['l']))
-print(formula(cmds, cmds["root"]['r']))
+def binary_search(tree, low, high, x):
+    # Check base case
+    if high >= low:
+        mid = (high + low) // 2
+
+        val = solve(tree, mid)
+        print(mid, ' => ', val)
+
+        # If element is present at the middle itself
+        if val == x:
+            return mid
+
+        elif val > x:
+            return binary_search(tree, low, mid - 1, x)
+        else:
+            return binary_search(tree, mid + 1, high, x)
+    else:
+        return -1
+
+tree = build_tree(cmds, 'root')
+print(solve(tree, 2313))
+
+r = solve(tree['r'])
+print(binary_search(tree['l'], 0, 1e16, r))
 
